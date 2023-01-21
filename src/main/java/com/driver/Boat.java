ebbc9a0d02dd3d710397669ee6fadc1181b3f18d
uttamshukla259 <uttamshukla259@gmail.com>
2023-01-21 16:29:06 +0530

    2 problem

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -1,19 +1,24 @@
 package com.driver;
 
-public class Boat implements WaterVehicle {
-    private String name;
-    private int capacity;
-
+public class Boat implements WaterVehicle{
+    String name;
+    int capacity;
     public Boat(String name, int capacity) {
         this.name = name;
         this.capacity = capacity;
     }
-
-    public String getVehicleName() {
-        return name;
+    public void setName(String name) {
+        this.name = name;
     }
-
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+    @Override
+    public String getVehicleName() {
+        return this.name;
+    }
+    @Override
     public int getVehicleCapacity() {
-        return capacity;
+        return this.capacity;
     }
 }
--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -28,45 +28,122 @@ package com.driver;
 //        }
 //    }
 //}
+//public class F1 extends Car {
+//
+//    public F1(String name, boolean isManual) {
+//        super(name, 4, 2, 6, isManual, "F1", 1);
+//    }
+//
+//    public void accelerate(int rate){
+//        int currentSpeed = 0;
+//        int newSpeed = currentSpeed + rate;
+//        int currentGear;
+//        if(newSpeed == 0) {
+//            stop();
+//            currentGear = 1;
+//        }
+//        else if (newSpeed > 0 && newSpeed <= 50) {
+//            currentGear = 1;
+//        }
+//        else if (newSpeed > 50 && newSpeed <= 100) {
+//            currentGear = 2;
+//        }
+//        else if (newSpeed > 100 && newSpeed <= 150) {
+//            currentGear = 3;
+//        }
+//        else if (newSpeed > 150 && newSpeed <= 200) {
+//            currentGear = 4;
+//        }
+//        else if (newSpeed > 200 && newSpeed <= 250) {
+//            currentGear = 5;
+//        }
+//        else {
+//            currentGear = 6;
+//        }
+//
+//        if(newSpeed > 0) {
+//            changeSpeed(newSpeed, getCurrentDirection());
+//        }
+//    }
+//
+//    private int getCurrentDirection() {
+//        return 0;
+//    }
+//}
+//public class F1 extends Car {
+//
+//    public F1(String name, boolean isManual) {
+//        super(name, 4, 2, 6, isManual, "F1", 1);
+//    }
+//
+//    public void accelerate(int rate){
+//        int newSpeed = currentSpeed + rate;
+//        if(newSpeed == 0) {
+//            stop();
+//            currentGear = 1;
+//        }
+//        else if (newSpeed > 0 && newSpeed <= 50) {
+//            currentGear = 1;
+//        }
+//        else if (newSpeed > 50 && newSpeed <= 100) {
+//            currentGear = 2;
+//        }
+//        else if (newSpeed > 100 && newSpeed <= 150) {
+//            currentGear = 3;
+//        }
+//        else if (newSpeed > 150 && newSpeed <= 200) {
+//            currentGear = 4;
+//        }
+//        else if (newSpeed > 200 && newSpeed <= 250) {
+//            currentGear = 5;
+//        }
+//        else {
+//            currentGear = 6;
+//        }
+//
+//        if(newSpeed > 0) {
+//            changeSpeed(newSpeed, getCurrentDirection());
+//        }
+//    }
+//}
 public class F1 extends Car {
-
+    String name;
+    boolean isManual;
     public F1(String name, boolean isManual) {
-        super(name, 4, 2, 6, isManual, "F1", 1);
+        //Use arbitrary values for parameters which are not mentioned
+        super(name,4, 4, 6, isManual,name,4);
     }
-
     public void accelerate(int rate){
-        int currentSpeed = 0;
-        int newSpeed = currentSpeed + rate;
-        int currentGear;
-        if(newSpeed == 0) {
-            stop();
-            currentGear = 1;
-        }
-        else if (newSpeed > 0 && newSpeed <= 50) {
-            currentGear = 1;
-        }
-        else if (newSpeed > 50 && newSpeed <= 100) {
-            currentGear = 2;
-        }
-        else if (newSpeed > 100 && newSpeed <= 150) {
-            currentGear = 3;
-        }
-        else if (newSpeed > 150 && newSpeed <= 200) {
-            currentGear = 4;
-        }
-        else if (newSpeed > 200 && newSpeed <= 250) {
-            currentGear = 5;
-        }
-        else {
-            currentGear = 6;
-        }
+        int newSpeed = getCurrentSpeed() + rate; //set the value of new speed  by using currentSpeed and rate
 
+        /**
+         * speed 0: gear 1
+         * speed 1-50: gear 1
+         * speed 51-100: gear 2
+         * speed 101-150: gear 3
+         * speed 151-200: gear 4
+         * speed 201-250: gear 5
+         * speed more than 250: gear 6
+         */
+        if(newSpeed == 0) {
+            //Stop the car, set gear as 1
+            changeGear(1);
+        }
+        else if(newSpeed>=1 && newSpeed<=50)
+            changeGear(1);
+        else if(newSpeed > 50 && newSpeed <= 100)
+            changeGear(2);
+        else if(newSpeed > 100 && newSpeed <= 150)
+            changeGear(3);
+        else if(newSpeed > 150 && newSpeed <= 200)
+            changeGear(4);
+        else if(newSpeed > 200 && newSpeed <= 250)
+            changeGear(5);
+        else if(newSpeed > 250)
+            changeGear(6);
+        //for all other cases, change the gear accordingly
         if(newSpeed > 0) {
             changeSpeed(newSpeed, getCurrentDirection());
         }
-    }
-
-    private int getCurrentDirection() {
-        return 0;
     }
 }
